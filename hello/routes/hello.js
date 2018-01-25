52409e27be01f92a44c183f8d58831545d2c39bd
Anthony Muzzillo <amuzzillo34@gmail.com>
2018-01-25 10:44:25 -0800

    Added hello

---
+++ hello/routes/hello.js
@@ -0,0 +1,17 @@
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+
+  var nameToShow = req.params.userName;
+  if(nameToShow === undefined){
+  		nameToShow = 'World';
+}
+
+  console.log('Name is' + nameToShow);
+  res.render('index', {
+  	'name': nameToShow
+  });
+};
