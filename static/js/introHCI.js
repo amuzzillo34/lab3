8f769165c0cb2ba630494b42106c183f14b983d7
Anthony Muzzillo <amuzzillo34@gmail.com>
2018-01-25 10:24:43 -0800

    Lab 3 in all its glory

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
@@ -19,7 +21,17 @@ function initializePage() {
 }
 
 function projectClick(e){
+	//console.log("Project Clicked");
 	e.preventDefault();
-
 	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if(description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project. </p> </div>");
+	} 
+	else {
+		description.fadeOut();
+	}
 }
